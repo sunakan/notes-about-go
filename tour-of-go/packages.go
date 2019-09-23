e3c75dfab9e8d0b48ff0dad55a6f454ea26b8f4f
sunakan <sunakan03@gmail.com>
2019-09-23 20:46:31 +0900

    [Add] Tour of go の最初らへん

---
+++ tour-of-go/packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("好きな数字は", rand.Intn(1000))
+}
---
+++ tour-of-go/sandbox.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("今の時間は", time.Now())
+}
